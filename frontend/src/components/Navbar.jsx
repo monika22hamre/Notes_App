2302c834eb3357bc1296a94cca791d76a9d9350d
monika22hamre <monika22hamre@gmail.com>
2025-06-17 23:07:56 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,45 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../context/ContextProvider';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useAuth } from "../context/ContextProvider";
 
 const Navbar = ({ setQuery }) => {
-    const { user, logout } = useAuth();
+  const { user, logout } = useAuth();
 
+  return (
+    <nav className="bg-gray-800 p-4 text-white flex justify-between items-center">
+      <div className="text-xl font-bold">
+        <Link to="/">NoteApp</Link>
+      </div>
+      <input
+        type="text"
+        placeholder="Search notes..."
+        className="bg-gray-600 px-4 py-2 rounded"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div>
+        {!user ? (
+          <>
+            <Link to="/login" className="bg-blue-500 px-4 py-2 rounded mr-4">
+              Login
+            </Link>
+            <Link to="/signup" className="bg-green-500 px-4 py-2 rounded mr-4">
+              Signup
+            </Link>
+          </>
+        ) : (
+          <>
+            <span className="mr-4">{user.name}</span>
 
-    return (
-        <nav className='bg-gray-800 p-4 text-white flex justify-between items-center'>
-            <div className='text-xl font-bold'>
-                <Link to="/">NoteApp</Link>
-            </div>
-            <input type="text"
-                placeholder='Search notes...'
-                className='bg-gray-600 px-4 py-2 rounded'
-                onChange={(e) => setQuery(e.target.value)}
-
-            />
-            <div>
-                {!user ? (
-                    <>
-                        <Link to="/login" className='bg-blue-500 px-4 py-2 rounded mr-4'>
-                            Login
-                        </Link>
-                        <Link to="/signup" className='bg-green-500 px-4 py-2 rounded mr-4'>
-                            Signup
-                        </Link>
-                    </>
-                ) : (
-                    <>
-                        <span className='mr-4'>{user.name}</span>
-
-                        <button className='bg-red-500 px-4 py-2 rounded' onClick={logout}>
-                            Logout
-                        </button>
-                    </>
-                )}
-            </div>
-        </nav>
-    );
+            <button className="bg-red-500 px-4 py-2 rounded" onClick={logout}>
+              Logout
+            </button>
+          </>
+        )}
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
